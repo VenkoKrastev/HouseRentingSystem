a65e52be1a395030a5837695ea0620318cd0ca51
Venelin Krastev <venelinkrstev@gmail.com>
2024-03-18 22:20:40 +0200

    Models - Category

---
+++ HouseRentingSystem.Infrastructure/Data/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using static HouseRentingSystem.Infrastructure.ConstantsDb.DataConstants;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+    [Comment("House category")]
+    public class Category
+    {
+        [Key]
+        [Comment("Category Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameLength)]
+        public string Name { get; set; } = string.Empty;
+
+        public List<House> Houses { get; set; } = new List<House>();
+    }
+}
