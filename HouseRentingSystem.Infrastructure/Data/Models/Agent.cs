bf4810829a860752cc979bcd7b3c67c9e8c827f7
Venelin Krastev <venelinkrstev@gmail.com>
2024-03-18 22:52:09 +0200

    Db

---
+++ HouseRentingSystem.Infrastructure/Data/Models/Agent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static HouseRentingSystem.Infrastructure.ConstantsDb.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+    [Comment("House Agent")]
+    public class Agent
+    {
+        [Key]
+        [Comment("Agent identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(AgentPhoneNumberMaxLength)]
+        [Comment("Agent's phone")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("User Identifier")]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; } = null!;
+    }
+}
+//•	Id – a unique integer, Primary Key
+//•	PhoneNumber – a string with min length 7 and max length 15 (required)
+//•	UserId – a string (required)
+//•	User – an IdentityUser object
---
+++ HouseRentingSystem.Infrastructure/Data/Models/House.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static HouseRentingSystem.Infrastructure.ConstantsDb.DataConstants;
+
+namespace HouseRentingSystem.Infrastructure.Data.Models
+{
+    [Comment("House to rent")]
+    public class House
+    {
+        [Key]
+        [Comment("House Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(HouseTitleMaxLength)]
+        [Comment("Title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(HouseAddressMaxLength)]
+        [Comment("House address")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(HouseDescriptionMaxLength)]
+        [Comment("House description")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("House image url")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required]
+        //[Range(typeof(decimal), HouseRentingPriceMin, HouseRentingPriceMax, ConvertValueInInvariantCulture = true )]
+        [Comment("Renting price for month")]
+        public decimal PricePerMonth { get; set; }
+
+        [Required]
+        [Comment("Category identifier")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Comment("Agent identifier")]
+        public int AgentId { get; set; }
+
+        [Comment("User id of the renterer")]
+        public string? RenterId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+        [ForeignKey(nameof(AgentId))]
+        public Agent Agent { get; set; } = null!;
+    }
+}
+
+
